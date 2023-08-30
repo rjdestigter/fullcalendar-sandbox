9c1f0ebe0132252f19e69c9c4a41b432ba07ebe5
John de Stigter <john.s@getjobber.com>
2023-08-30 11:07:00 -0600

    foobar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { EventInput } from '@fullcalendar/core';
+
+const plugins = [timeGridPlugin, dayGridPlugin];
+
+let eventGuid = 0;
+let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: createEventId(),
+    title: "Timed event",
+    start: todayStr + "T12:30:00",
+    end: todayStr + "T01:00:00",
+  },
+  {
+    id: createEventId(),
+    title: "Timed event",
+    start: todayStr + "T12:00:00",
+    end: todayStr + "T02:00:00",
+  },
+];
+
+export function createEventId() {
+  return String(eventGuid++);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <FullCalendar
+        ref={(ref) => {
+          // ref?.getApi().select('2021-05-01')
+        }}
+        plugins={plugins}
+        initialView="timeGridWeek"
+        dayHeaders
+        initialEvents={INITIAL_EVENTS}
+        eventContent={renderEventContent}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "timeGridWeek,timeGridDay,dayGridMonth",
+        }}
+      />
+    </div>
+  );
+}
+
+function renderEventContent(eventInfo: any) {
+  return (
+    <div>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
     </div>
   );
 }
